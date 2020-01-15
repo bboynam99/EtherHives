0f4a6ca683c9c7157b34a4f7bd0aa749127f4fd2
Developer <dev@beebee.com>
2020-01-15 15:31:56 +0300

    Add test

--- test/EtherHives.js
+++ test/EtherHives.js
@@ -1,12 +1,36 @@
-// const { expectRevert } = require('openzeppelin-test-helpers');
+const { time } = require('openzeppelin-test-helpers');
 // const { expect } = require('chai');
 
 const EtherHives = artifacts.require('EtherHives');
 
-contract('EtherHives', function ([_, addr1]) {
+async function timeIncreaseTo (seconds) {
+    const delay = 1000 - new Date().getMilliseconds();
+    await new Promise(resolve => setTimeout(resolve, delay));
+    await time.increaseTo(seconds);
+}
+
+contract('EtherHives', function ([_, wallet1, wallet2, wallet3]) {
     describe('EtherHives', async function () {
-        it('should be ok', async function () {
-            this.token = await EtherHives.new();
+        beforeEach(async function () {
+            this.hive = await EtherHives.new();
+            this.started = (await time.latest()).addn(10);
+            await timeIncreaseTo(this.started);
+        });
+
+        it('test', async function () {
+            this.hive.deposit('0x0000000000000000000000000000000000000000', { value: web3.utils.toWei('0'), from: wallet1 });
+            this.hive.deposit('0x0000000000000000000000000000000000000000', { value: web3.utils.toWei('0'), from: wallet2 });
+            this.hive.deposit('0x0000000000000000000000000000000000000000', { value: web3.utils.toWei('0'), from: wallet3 });
+
+            this.hive.deposit('0x0000000000000000000000000000000000000000', { value: web3.utils.toWei('1'), from: wallet1 });
+            // this.hive.deposit('0x0000000000000000000000000000000000000000', { value: web3.utils.toWei('2'), from: wallet2 });
+            // this.hive.deposit('0x0000000000000000000000000000000000000000', { value: web3.utils.toWei('3'), from: wallet3 });
+
+            await timeIncreaseTo(this.started.add(time.duration.weeks(1)));
+
+            // this.hive.deposit('0x0000000000000000000000000000000000000000', { value: web3.utils.toWei('1'), from: wallet1 });
+            // this.hive.deposit('0x0000000000000000000000000000000000000000', { value: web3.utils.toWei('2'), from: wallet2 });
+            // this.hive.deposit('0x0000000000000000000000000000000000000000', { value: web3.utils.toWei('3'), from: wallet3 });
         });
     });
 });
